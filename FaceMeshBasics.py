5af37eaf8dc59f31b8d6046c2cbc412e6fb348c7
DESKTOP-EAISUPF\black <blacksw1102@gmail.com>
2021-12-14 10:51:53 +0900

    얼굴 인식 연습 샘플

---
+++ FaceMeshBasics.py
@@ -0,0 +1,33 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture("Videos/3.mp4")
+pTime = 0
+
+mpDraw = mp.solutions.drawing_utils
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
+drawSpec = mpDraw.DrawingSpec(thickness=2, circle_radius=2)
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
+
+    if results.multi_face_landmarks:
+        for faceLms in results.multi_face_landmarks:
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_FACE_OVAL, drawSpec, drawSpec)
+
+            for id, lm in enumerate(faceLms.landmark):
+                ih, iw, ic = img.shape
+                x, y = int(lm.x * iw), int(lm.y * ih)
+                print(id, x, y)
+
+    cTime = time.time()
+    fps = 1 / (cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+    cv2.imshow("Image", cv2.resize(img, (960, 540)))
+    cv2.waitKey(1)
+
---
+++ FaceMeshModule.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class FaceMeshDetector():
+
+    def __init__(self, staticMode=False, maxFaces=2, minDetectionCon=0.5, minTrackCon=0.5):
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.mpFaceMesh.FaceMesh(static_image_mode=self.staticMode,
+                                                 max_num_faces=self.maxFaces,
+                                                 min_detection_confidence=self.minDetectionCon,
+                                                 min_tracking_confidence=self.minTrackCon)
+        self.drawSpec = self.mpDraw.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1)
+
+    def findFaceMesh(self, img, draw=True):
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(self.imgRGB)
+        faces = []
+
+        if self.results.multi_face_landmarks:
+            for faceLms in self.results.multi_face_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_FACE_OVAL,
+                                           self.drawSpec, self.drawSpec)
+
+                face = []
+                for id, lm in enumerate(faceLms.landmark):
+                    ih, iw, ic = img.shape
+                    x, y = int(lm.x * iw), int(lm.y * ih)
+                    #cv2.putText(img, str(id), (x, y), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
+                    face.append([x,y])
+
+                faces.append(face)
+
+        return img, faces
+
+
+def main():
+    # use by video sample
+    #cap = cv2.VideoCapture("Videos/3.mp4")
+
+    # use by cam video
+    cap = cv2.VideoCapture(0)
+
+    pTime = 0
+    detector = FaceMeshDetector(maxFaces=2)
+    while True:
+        success, img = cap.read()
+        img, faces = detector.findFaceMesh(img=img, draw=True)
+        if len(faces) != 0:
+            print(faces,"\n")
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 5, (0, 255, 0), 3)
+        cv2.imshow("Image", cv2.resize(img, (960, 540)))
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
